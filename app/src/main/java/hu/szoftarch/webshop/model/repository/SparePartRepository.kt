d3ebe3448bcc264a5e07c313a4e3c7f95cbdad78
Kristóf Pásztor <pasztor.kristof.kp@gmail.com>
2024-09-14 15:15:26 +0200

    Delete SparePart related components

--- app/src/main/java/hu/szoftarch/webshop/model/repository/SparePartRepository.kt
+++
@@ -1,7 +0,0 @@
-package hu.szoftarch.webshop.model.repository
-
-import hu.szoftarch.webshop.model.data.SparePart
-
-interface SparePartRepository {
-    suspend fun getSparePartById(id: Long): SparePart?
-}
