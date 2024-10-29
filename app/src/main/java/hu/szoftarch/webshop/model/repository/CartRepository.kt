4270557ff0f4f8c29c0affebe13bcad5bfdf520b
bgyorfi <bence.gyorfi119@gmail.com>
2024-10-29 17:14:40 +0100

    Add quantity based changes to CartRepo

--- app/src/main/java/hu/szoftarch/webshop/model/repository/CartRepository.kt
+++ app/src/main/java/hu/szoftarch/webshop/model/repository/CartRepository.kt
@@ -3,9 +3,9 @@ package hu.szoftarch.webshop.model.repository
 import hu.szoftarch.webshop.model.data.CartContent
 
 interface CartRepository {
-    suspend fun addToCart(productId: Int): CartContent
+    suspend fun addToCart(productId: Int, quantity: Int): CartContent
 
-    suspend fun removeFromCart(productId: Int): CartContent
+    suspend fun removeFromCart(productId: Int,  quantity: Int): CartContent
 
     suspend fun getProductsInCart(): CartContent
 
