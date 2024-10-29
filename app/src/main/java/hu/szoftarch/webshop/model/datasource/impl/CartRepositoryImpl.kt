fda38da745354a1604b7c3a0a4fd9b3b96171d2b
bgyorfi <bence.gyorfi119@gmail.com>
2024-10-29 17:12:05 +0100

    Add quantity based cart add and cart remove logic

--- app/src/main/java/hu/szoftarch/webshop/model/datasource/impl/CartRepositoryImpl.kt
+++ app/src/main/java/hu/szoftarch/webshop/model/datasource/impl/CartRepositoryImpl.kt
@@ -6,13 +6,27 @@ import hu.szoftarch.webshop.model.repository.CartRepository
 object CartRepositoryImpl : CartRepository {
     private var cartContent = CartContent(products = mapOf())
 
-    override suspend fun addToCart(productId: Int): CartContent {
-        cartContent = cartContent.add(productId)
+     override suspend fun addToCart(productId: Int, quantity: Int): CartContent {
+        val currentQuantity = cartContent.products[productId] ?: 0
+        cartContent = cartContent.copy(
+            products = cartContent.products + (productId to (currentQuantity + quantity))
+        )
         return cartContent.copy()
     }
 
-    override suspend fun removeFromCart(productId: Int): CartContent {
-        cartContent = cartContent.remove(productId)
+    override suspend fun removeFromCart(productId: Int, quantity: Int): CartContent {
+        val currentQuantity = cartContent.products[productId] ?: 0
+        cartContent = if (currentQuantity > quantity) {
+            cartContent.copy(
+                products = cartContent.products.toMutableMap().apply {
+                    this[productId] = currentQuantity - quantity
+                }
+            )
+        } else {
+            cartContent.copy(
+                products = cartContent.products - productId
+            )
+        }
         return cartContent.copy()
     }
 
