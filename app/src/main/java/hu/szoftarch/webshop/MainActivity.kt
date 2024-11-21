dee13bc1880f9d95494d818bdc020bc53897800e
bgyorfi <bence.gyorfi119@gmail.com>
2024-11-21 23:51:19 +0100

    add intenturi param from main activity through navgraph to searchscreen

--- app/src/main/java/hu/szoftarch/webshop/MainActivity.kt
+++ app/src/main/java/hu/szoftarch/webshop/MainActivity.kt
@@ -1,6 +1,8 @@
 package hu.szoftarch.webshop
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,9 +15,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val intentUri = intent?.data
+        Log.d("MainActivity", "onCreate called with URI: $intentUri")
         setContent {
             WebshopTheme {
-                NavGraph()
+                NavGraph(startingIntentUri = intentUri)
             }
         }
     }
