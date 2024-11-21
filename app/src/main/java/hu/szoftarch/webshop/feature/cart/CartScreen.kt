7ec736224ebd78ffed0b40f6a769eac7d9f90142
bgyorfi <bence.gyorfi119@gmail.com>
2024-11-21 23:49:34 +0100

    add delete cart button

--- app/src/main/java/hu/szoftarch/webshop/feature/cart/CartScreen.kt
+++ app/src/main/java/hu/szoftarch/webshop/feature/cart/CartScreen.kt
@@ -13,14 +13,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Payment
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
@@ -34,8 +37,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import hu.szoftarch.webshop.ui.common.ProductCardWithAddRemove
@@ -106,7 +111,8 @@ fun CartScreen(
                     start = 32.dp,
                     end = 32.dp
                 ),
-            total = cartViewModel.total
+            total = cartViewModel.total,
+            onClearCart = { cartViewModel.clearCart() }
         )
     }
 }
@@ -116,37 +122,50 @@ fun CartScreen(
 @Composable
 private fun CheckoutButton(
     modifier: Modifier = Modifier,
-    total: Int
+    total: Int,
+    onClearCart: () -> Unit
 ) {
     val sheetState = rememberModalBottomSheetState()
     var showBottomSheet by remember { mutableStateOf(false) }
 
-    FloatingActionButton(
-        modifier = modifier,
-        onClick = {
-            showBottomSheet = true
-        }
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+        Button(
+            onClick = onClearCart,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFB42147)
+            )
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Delete,
+                contentDescription = "",
+                modifier = Modifier.size(24.dp)
+            )
+        }
+
+        Button(
+            onClick = { showBottomSheet = true },
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF198605)
+            )
         ) {
             Text(
-                modifier = Modifier.weight(1f),
                 text = "$total HUF",
                 fontWeight = FontWeight.Bold
             )
-
+            Spacer(modifier = Modifier.width(8.dp))
             Icon(
                 imageVector = Icons.Filled.Payment,
-                contentDescription = null
+                contentDescription = "Check Out",
+
             )
-
             Spacer(modifier = Modifier.width(8.dp))
-
             Text(text = "Check Out")
         }
     }
